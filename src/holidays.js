59bdb5d9efaa444a2c2b9770ec59496cfa9d5a05
arabeuna <arabuenapecanhagomes@gmail.com>
2025-05-04 22:27:39 -0300

    Inclusão dos feriados

--- src/holidays.js
+++ src/holidays.js
@@ -1,17 +1,17 @@
 // holidays.js
 const holidays = [
-  '2024-01-01', // Ano Novo
-  '2024-02-12', // Carnaval
-  '2024-02-13', // Carnaval
-  '2024-03-29', // Sexta-feira Santa
-  '2024-04-21', // Tiradentes
-  '2024-05-01', // Dia do Trabalho
-  '2024-05-30', // Corpus Christi
-  '2024-09-07', // Independência
-  '2024-10-12', // Nossa Senhora
-  '2024-11-02', // Finados
-  '2024-11-15', // Proclamação da República
-  '2024-12-25', // Natal
+  '2025-01-01', // Ano Novo
+  '2025-02-12', // Carnaval
+  '2025-02-13', // Carnaval
+  '2025-03-29', // Sexta-feira Santa
+  '2025-04-21', // Tiradentes
+  '2025-05-01', // Dia do Trabalho
+  '2025-05-30', // Corpus Christi
+  '2025-09-07', // Independência
+  '2025-10-12', // Nossa Senhora
+  '2025-11-02', // Finados
+  '2025-11-15', // Proclamação da República
+  '2025-12-25', // Natal
 ];
 
 export const isHoliday = (date) => {
