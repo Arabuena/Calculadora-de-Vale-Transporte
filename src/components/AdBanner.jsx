b805d804f65cd5204f87eda44befa43f22ae4de6
arabeuna <arabuenapecanhagomes@gmail.com>
2025-05-06 17:18:37 -0300

    ççç

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const AdBanner = () => {
-  useEffect(() => {
-    // Remover script existente se houver
-    const existingScript = document.querySelector('script[src*="profitableratecpm.com"]');
-    if (existingScript) {
-      existingScript.remove();
-    }
-
-    // Criar e adicionar novo script
-    const script = document.createElement('script');
-    script.async = true;
-    script.setAttribute('data-cfasync', 'false');
-    script.src = '//pl26576496.profitableratecpm.com/dc2ddb957cfec84d81047be88a201264/invoke.js';
-    document.body.appendChild(script);
-  }, []);
-
   return (
-    <div id="container-dc2ddb957cfec84d81047be88a201264"></div>
+    <div className="ad-container">
+      <div id="container-dc2ddb957cfec84d81047be88a201264"></div>
+    </div>
   );
 };
 
