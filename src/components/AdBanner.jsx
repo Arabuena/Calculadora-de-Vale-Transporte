2ad75f28bb864b047d184b0b805d2d170ba8dc1c
arabeuna <arabuenapecanhagomes@gmail.com>
2025-05-07 10:24:32 -0300

    baner right include

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 const AdBanner = () => {
   return (
-    <div className="side-banner-container">
+    <div className="horizontal-banner-container">
       <a 
         href="https://www.profitableratecpm.com/ibs5zaqf?key=be17333b70911d1e3d3fe6b73762de0f"
         rel="nofollow" 
         target="_blank"
-        className="side-banner-link"
       >
         <img 
           alt="banner" 
-          src="https://landings-cdn.adsterratech.com/referralBanners/gif/120x600_adsterra_reff.gif"
-          className="side-banner-image"
+          src="https://landings-cdn.adsterratech.com/referralBanners/png/728%20x%2090%20px.png"
+          className="horizontal-banner-image"
         />
       </a>
     </div>
